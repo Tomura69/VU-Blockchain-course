85842f615ffdea64408b446c08ad68f0cf55ebdf
Tomas <tomas.klevas@mif.vu.lt>
2019-10-03 23:08:31 +0300

    Galutinis

--- Hashas.cpp
+++ Hashas.cpp
@@ -8,23 +8,23 @@
 #include <sstream>
 #include <stdio.h>
 #include <string.h>
+#include <chrono>
 #include <algorithm>
 #include <iterator> 
 
 using std::cout;
 using std::endl;
 
-void Hash (long long hashvalue, std::vector<int> &hashas);
+void Hash (long long hashvalue, std::string & hashFinal);
 void Skaitymas (long long & hashvalue);
 
 int main(int argc, char const *argv[]){
-	std::string input;
+	std::string input, hashFinal;
 	int choice;
-	std::vector<int> hashas;
 	long long hashvalue = 0;
 
 	while (true){
-		cout << "Jei norite skaityti duomenis is failo spauskite 1, jei norite ivesti spauskite 2" << endl;
+		cout << "Jei norite skaityti duomenis is failu spauskite 1, jei norite ivesti spauskite 2" << endl;
 		std::cin >> choice;
 		if (choice == 1 || choice == 2){
 			break;
@@ -41,58 +41,97 @@ int main(int argc, char const *argv[]){
 	if (choice == 1){
 		Skaitymas(hashvalue);
 	}
-	Hash(hashvalue, hashas);
-	// nuo 58 iki 64 istrinti ir nuo 91 iki 96
-	for (int i = 0; i < hashas.size(); i++){
-		if (hashas[i] > 57 && hashas[i] < 65){
-			hashas[i] = hashas[i] - 9;
-		}
-		if (hashas[i] > 90 && hashas[i] < 97){
-			hashas[i] = hashas[i] + 9;
-		}
-	}
 
-	char c;
-	cout << "HASH_VALUE: ";
-	for (int i = 0; i < hashas.size(); i++){
-		c = hashas[i];
-		cout << c;
-	}
-	cout << endl;
+	Hash(hashvalue, hashFinal);
+
+	cout << "HASH_VALUE: " << hashFinal << endl;
 	return 0;
 }
 
 void Skaitymas (long long & hashvalue){
-	std::ifstream fd("text.md");
-	std::string input;
+	int uzduotis;
 
-	if (!fd) {
-    cout << "Failas neegzistuoja";
-    exit(1);
+	while (true) {
+		cout << "Pasirinkite, kuri faila nuskaityti:" << endl;
+		cout << "1-as - Vienas simbolis" << endl;
+		cout << "2-as - Vienas simbolis, bet skirtingas" << endl;
+		cout << "3-as - 100000 symboliu tekstas" << endl;
+		cout << "4-as - Daugiau nei 100000 symboliu tekstas" << endl;
+		cout << "5-as - Tekstas" << endl;
+		cout << "6-as - Tekstas, su vienu skirtingu simboliu" << endl;
+		cout << "7-as - Tuscias failas" << endl;
+		cout << "8-as - Konstitucijos eiluciu hashinimas" << endl;
+		std::cin >> uzduotis;
+		if (uzduotis >= 1 && uzduotis <= 8) break;
+		else cout << "Blogai pasirinkote faila" << endl;
 	}
 
-	std::cin.ignore();
-	while (!fd.eof()){
-		getline(fd, input);
-		for (int i = 0; i < input.size(); i++){
-			hashvalue = hashvalue + int(input[i]);
+    if (uzduotis == 8){
+    	std::ifstream fd("konstitucija.md");
+		std::string input, hashFinal;
+		std::vector<int> hashas;
+		hashvalue = 0;
+
+		if (!fd) {
+	    cout << "Failas neegzistuoja" << endl;
+	    exit(1);
 		}
+		double suma = 0;
+		std::cin.ignore();
+		while (!fd.eof()){
+			getline(fd, input);
+			for (int i = 0; i < input.size(); i++){
+				hashvalue = hashvalue + int(input[i]);
+			}
+
+			auto startas = std::chrono::system_clock::now();
+			Hash(hashvalue, hashFinal);
+			auto pabaiga = std::chrono::system_clock::now();
+    		auto uztruko = std::chrono::duration_cast<
+    		std::chrono::duration<double> >(pabaiga - startas).count();
+    		suma = suma + uztruko;
+		}
+		cout << "Konstitucijos eiluciu hashavimas uztruko: " << suma << " sekundziu" << endl;
+		fd.close();
+    }
+
+    else{
+		std::string s = std::to_string(uzduotis);
+		std::ifstream fd("test" + s + ".md");
+		std::string input;
+
+		if (!fd) {
+	    cout << "Failas neegzistuoja" << endl;
+	    exit(1);
+		}
+
+		std::cin.ignore();
+		while (!fd.eof()){
+			getline(fd, input);
+			for (int i = 0; i < input.size(); i++){
+				hashvalue = hashvalue + int(input[i]);
+			}
+		}
+		fd.close();
 	}
-	fd.close();
 }
 
-void Hash (long long hashvalue, std::vector<int> &hashas){
+void Hash (long long hashvalue, std::string & hashFinal){
 	//Paverti kiekviena simboli i skaiciu, tuos skaicius sudedi, padaugini is kazko, kad gauti kosmosa,
 	//padalint i dalis, kad gaut 64 simbolius
 	//char kodai (skaiciai ir raides) nuo 48 iki 122
 	hashvalue = hashvalue * 5468;
+	hashFinal = "";
 	if (hashvalue % 10 == 0) hashvalue++;
 	long long tarp;
 	int tarp2;
+	int apkeistas;
 	int betkas;
+	std::vector<int> hashas;
 	for (int i = 0; i < 32; i++){
 		tarp = (i + 1) * 57;
-		tarp2 = (hashvalue * tarp) % 100;
+		apkeistas = (hashvalue * tarp) % 100;
+		tarp2 = (apkeistas % 10) * 10 + (apkeistas / 10);
 		if (tarp2 == 1 || tarp2 == 2){
 			hashas.push_back((double)tarp2 * 53);
 			continue;
@@ -115,4 +154,44 @@ void Hash (long long hashvalue, std::vector<int> &hashas){
 			continue;
 		}
 	}
+	for (int i = 0; i < hashas.size(); i++){
+		if (hashas[i] > 57 && hashas[i] <= 67){
+			hashas[i] = hashas[i] - 10;
+			continue;
+		}
+		if (hashas[i] > 67 && hashas[i] <= 77){
+			hashas[i] = hashas[i] - 20;
+			continue;
+		}
+		if (hashas[i] > 77 && hashas[i] <= 87){
+			hashas[i] = hashas[i] - 30;
+			continue;
+		}
+		if (hashas[i] > 87 && hashas[i] <= 92){
+			hashas[i] = hashas[i] + 10;
+			continue;
+		}
+		if (hashas[i] > 92 && hashas[i] < 97){
+			hashas[i] = hashas[i] + 6;
+			continue;
+		}
+		if (hashas[i] > 102 && hashas[i] <= 108){
+			hashas[i] = hashas[i] - 6;
+			continue;
+		}
+		if (hashas[i] > 108 && hashas[i] <= 114){
+			hashas[i] = hashas[i] - 12;
+			continue;
+		}
+		if (hashas[i] > 114 && hashas[i] <= 122){
+			hashas[i] = hashas[i]/2 - 6;
+			continue;
+		}
+	}
+	char c;
+	for (int i = 0; i < hashas.size(); i++){
+		c = hashas[i];
+		hashFinal = hashFinal + c;
+	}
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # VU-Blockchain-course
+
+## v0.1
+
+### Hash'avimo funkcijos idėja
+  * Idėja labai paprasta, inputo simbolius pasiversti skaičiais ir juos sudėti. Taip gaunamas unikalus skaičius, kuris priskirtas tam
+  inputui. Tada tas skaičius modifikuojamas ir išgaunamas vektorius, kuris saugo skaičius (49, 100, 57, 97). Tie skaičiai
+  yra paverčiami į `char` simbolius ir taip išgaunama hash'as.
+  * skaičius [48, 57]
+  * raidė [97, 102]
+
+### Programos naudojimas
+Naudoti programą yra labai paprasta:
+  * Atsisiųsti visus failus
+  * Atsidaryti `command line`
+  * Įveskite žodį `make`
+  * Toliau vykdykite instrukcijas koncolėje
+  
+### Analizė:
+#### 1 dalis
+
+1. Užduotis: du failai su vienu skirtingu simboliu
+
+| Failai  | Output |
+| ------------- | ------------- |
+| test1.md (L)  | 974b67120f56b50378fd126a974b671  |
+| test2.md (p) | 6a56f2012d1f7867b340759b6a56f20  |
+
+2. Užduotis: du failai su visiškai skirtingais tekstais (virš 10000 simbolių)
+
+| Failai  | Output |
+| ------------- | ------------- |
+| test3.md (99999 simbolių) | fb19d527100423fb6756a867fb19d52  |
+| test4.md (~100000 simbolių)  | b957043b7687f1d2102f65a6b957043  |
+
+3. Užduotis: failai skiriasi tik vienu simboiu
+
+| Inputas  | Output |
+| ------------- | ------------- |
+| test5.md (paprastas tekstas) | 576609e1752f7377f8009dfb49c5f560 |
+| test6.md (tekste pakeista paskutinė raidė)  | 61f70b501649a2d8356f27b761f70b5  |
+
+4. Užduotis: tuščias failas
+
+| Inputas  | Output |
+| ------------- | ------------- |
+| test7.md  | 7d3404c72540ec76f4b0146844c1e58a  |
+
+#### 2 dalis
+
+Ištirkite Jūsų sukurtos hash funkcijos efektyvumą: tuo tikslu suhash'uokite kiekvieną eilutę iš konstitucija.txt failo.
+
+| Bandymas  | Laikas |
+| ------------- | ------------- |
+| 1  | 0.006505 s |
+| 2  | 0.0065179 s  |
+| 3  | 0.0071385 s |
